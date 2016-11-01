42ec09b24863575c1fa39cbd9862bbfa446b501c
Clark Sanford <clrksanford@gmail.com>
2016-11-01 18:00:20 -0500

    Did homework

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor() {
+    super();
+    this.state = {
+      balance: 0
+    }
+  }
+
+  _handleDepositClick() {
+    let amount = +this.refs.amount.value;
+    let currentBalance = this.state.balance;
+
+    this.setState({
+      balance: amount + currentBalance
+    });
+
+    this.refs.amount.value = '';
+  }
+
+  _handleWithdrawClick() {
+    let amount = +this.refs.amount.value;
+    let currentBalance = this.state.balance;
+
+    if (amount > currentBalance) {
+      alert('You may not overdraw');
+    } else {
+      this.setState({
+        balance: currentBalance - amount
+      });
+    }
+
+    this.refs.amount.value = '';
+  }
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref="amount" />
+        <input type="button" value="Deposit" onClick={this._handleDepositClick.bind(this)} />
+        <input type="button" value="Withdraw" onClick={this._handleWithdrawClick.bind(this)} />
       </div>
     )
   }
